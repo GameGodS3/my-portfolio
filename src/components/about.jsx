60a9720b11deefd2e2e7b2d9de14439c5a43d281
AJAY K <ajayk@cet.ac.in>
2020-09-17 00:37:43 +0530

    Updated about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -18,28 +18,37 @@ export default function About(props) {
       <div id="about" className={classes.title} >
         <Typography variant="h2" style={{ flex:1, textAlign: 'center' }} >About</Typography>
         <Grid container direction='row' justify='center' spacing={3} >
-            <Grid item sm={12} md={9} style={{ marginTop: '38px', color: 'black' }} >
+            <Grid item sm={12} md={12} style={{ marginTop: '38px', color: 'black' }} >
                 {/* <Card  >
                     <CardContent > */}
-                        <Typography style={{ fontSize: '25px' }}>
-                           - cs student at college of engineering tvm.
+                        <Typography style={{ marginBottom: '18px', fontSize: '18px' }} >
+                            I'm a 2019-2023 batch engineering student pursuing Computer Science at
+                            College of Engineering, TVM.
+                            My primary passion and love in tech is for mobile app dev,
+                            backend design and just building cool projects!
                         </Typography>
-                        <Typography style={{ fontSize: '25px' }}>
-                           - tech enthusiast and crazy motorhead.
+                        <Typography style={{ marginBottom: '18px', fontSize: '18px' }} >
+                            Currently I am a Frontend developer, with knowledge and experience in
+                            the cross platform mobile app framework Flutter, and web framework React.
+                            My aim is to be a full stack developer, and I currently have some experience in backend 
+                            development using python frameworks like django and flask as well.
                         </Typography>
-                        <Typography style={{ fontSize: '25px' }}>
-                           - frontend mobile and web developer.
+                        <Typography style={{ marginBottom: '18px', fontSize: '18px' }} >
+                            I love open source and therefore have participated in many open source
+                            promoting workshops and programmes in college and is now the Project Co-ordinator
+                            of FOSSCell Cet Club. I strongly believe in the community tech culture and collaboration with its members
+                            and therefore try to involve myself in all tech related discussions and communities whenever
+                            possible.
                         </Typography>
-                        <Typography style={{ fontSize: '25px' }}>
-                           - i love open source!!
-                        </Typography>
-                        <Typography style={{ fontSize: '25px' }}>
-                           - currently working on Ceekyoo search engine project.
+                        <Typography style={{ marginBottom: '18px', fontSize: '18px' }} >
+                            I enjoy working and collabing with teams and am currently part of a student
+                            team, as the UX designer and mobile frontend developer, of Ceekyoo search engine 
+                            project.
                         </Typography>
                     {/* </CardContent>
                 </Card> */}
             </Grid>
-            <Grid  container direction='column' alignItems='center' item sm={12} md={3} 
+            {/* <Grid  container direction='column' alignItems='center' item sm={12} md={3} 
                 style={{ marginTop: '30px', color: 'black' }} >
                     <IconButton onClick={ () => window.open(pdf, '_blank') } >
                         <Avatar src={resume} style={{ height: '140px', width: '140px' }} />
@@ -52,7 +61,7 @@ export default function About(props) {
                     <CardMedia style={{ height: '180px', width: '280px' }} image={coder} ></CardMedia>
                 </Card>
             </Box>
-            <Grid item sm={false} md={12} ></Grid>
+            <Grid item sm={false} md={12} ></Grid> */}
             <Grid container justify='center' >
                 <Box border={1} borderColor='black' borderRadius='50%' >
                     <IconButton onClick={ () => scroll("skills") } >
